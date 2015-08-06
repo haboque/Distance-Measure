6d131a1617dc399681d029d966e4511d85308ca4
haboqueferus <haboqueferus346@gmail.com>
2015-08-06 17:44:40 +0800

    link change

--- firmware/Distance Measure.cpp
+++ firmware/Distance Measure.cpp
@@ -5,7 +5,7 @@
 // See "NewPing.h" for purpose, syntax, version history, links, and more.
 // ---------------------------------------------------------------------------
 
-#include "NewPing.h"
+#include "Distance Measure.h"
 
 
 // ---------------------------------------------------------------------------
